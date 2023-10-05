b250f8ae74bdaefa277910a49764fec06ae71cf7
prknezek <prknezek@gmail.com>
2023-10-05 01:46:23 -0500

    using alpha beta pruning nega max algorithm

--- SmartMoveFinder.py
+++ SmartMoveFinder.py
@@ -3,18 +3,13 @@ from random import randint, shuffle
 piece_score = {"K": 0, "Q": 9, "R": 5, "N": 3, "B": 3, "p": 1}
 CHECKMATE = 1000
 STALEMATE = 0
-DEPTH = 2
+DEPTH = 3
 
 '''
 Picks and returns a random move
 '''
 def find_random_move(valid_moves) :
     return valid_moves[randint(0, len(valid_moves) - 1)]
-
-'''
-Finds the best move based on material alone
-'''
-# def find_best_move(gs, valid_moves) :
     # turn_multiplier = 1 if gs.white_to_move else -1
     # opponent_minmax_score = CHECKMATE # from black's perspective, checkmate is the worst possible score
     # best_player_move = None
@@ -57,55 +52,33 @@ Helper method to make the first recursive call to find the best move
 def find_best_move(gs, valid_moves) :
     global next_move
     next_move = None
-    find_move_nega_max(gs, valid_moves, DEPTH, 1 if gs.white_to_move else -1)
+    find_move_nega_max_alpha_beta(gs, valid_moves, DEPTH, -CHECKMATE, CHECKMATE, 1 if gs.white_to_move else -1)
     return next_move
 
-def find_move_minmax(gs, valid_moves, depth, white_to_move) :
-    global next_move
-    # base cases
-    if depth == 0 :
-        return score_board(gs.board)
-
-    if white_to_move :
-        max_score = -CHECKMATE
-        for move in valid_moves :
-            gs.make_move(move)
-            next_moves = gs.get_valid_moves()
-            score = find_move_minmax(gs, next_moves, depth - 1, False)
-            if score > max_score :
-                max_score = score
-                if depth == DEPTH :
-                    next_move = move
-            gs.undo_move()
-        return max_score
-    else :
-        min_score = CHECKMATE
-        for move in valid_moves :
-            gs.make_move(move)
-            next_moves = gs.get_valid_moves()
-            score = find_move_minmax(gs, next_moves, depth - 1, True)
-            if score < min_score :
-                min_score = score
-                if depth == DEPTH :
-                    next_move = move
-            gs.undo_move()
-        return min_score
-            
-def find_move_nega_max(gs, valid_moves, depth, turn_multiplier) :
+'''
+Find the best move using the nega max alpha beta pruning algorithm
+'''
+def find_move_nega_max_alpha_beta(gs, valid_moves, depth, alpha, beta, turn_multiplier) :
     global next_move
     if depth == 0 :
         return turn_multiplier * score_board(gs)
 
+    # move ordering - implement later
     max_score = -CHECKMATE
     for move in valid_moves :
         gs.make_move(move)
         next_moves = gs.get_valid_moves()
-        score = -find_move_nega_max(gs, next_moves, depth - 1, -turn_multiplier)
+        score = -find_move_nega_max_alpha_beta(gs, next_moves, depth - 1, -beta, -alpha, -turn_multiplier)
         if score > max_score :
             max_score = score
             if depth == DEPTH :
                 next_move = move
         gs.undo_move()
+
+        if max_score > alpha : # PRUNING
+            alpha = max_score
+        if alpha >= beta :
+            break
     return max_score
 
 '''
@@ -129,18 +102,3 @@ def score_board(gs) :
                 score -= piece_score[square[1]]
     
     return score
-
-'''
-Score the board based on material
-'''
-def score_material(board) :
-    score = 0
-    for row in board :
-        for square in row :
-            if square[0] == 'w' :
-                score += piece_score[square[1]]
-            elif square[0] == 'b' :
-                score -= piece_score[square[1]]
-    
-    return score
-
